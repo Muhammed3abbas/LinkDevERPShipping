d72cc2699965fd4ab9b80014911e63fb189b46a0
Mohamed Abdelnaser Abbas <92081347+Muhammed3abbas@users.noreply.github.com>
2025-01-07 01:13:06 +0200

    first Shipping commit

---
+++ Shipping.Domain/Models/ShippingOrder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipping.Domain.Models
+{
+    public class ShippingOrder
+    {
+        //Define properties such as Id, ShippingNumber, Date, PalletCount, Items, PurchaseOrderId.
+    }
+}
---
+++ Shipping.Domain/Models/ShippingOrderItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipping.Domain.Models
+{
+    public class ShippingOrderItem
+    {
+        public string Code { get; set; }
+        public decimal Price { get; set; }
+        public int PalletCount { get; set; }
+    }
+}
