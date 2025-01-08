0c087f875c1e8f73009a003bc6d273df1b68a128
Mohamed Abdelnaser Abbas <92081347+Muhammed3abbas@users.noreply.github.com>
2025-01-08 09:07:32 +0200

    update References

--- Shipping.Domain/obj/Debug/net8.0/Shipping.Domain.AssemblyInfo.cs
+++ Shipping.Domain/obj/Debug/net8.0/Shipping.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shipping.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d72cc2699965fd4ab9b80014911e63fb189b46a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shipping.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shipping.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
